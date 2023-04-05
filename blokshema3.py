7ee19bb8c0f02ba47e52198cb16dc56f163ef830
Renārs Sirotins <renars.sirotins@ventspils.edu.lv>
2023-04-05 11:36:17 +0300

    pabeigta 3. blokshēma

--- blokshema3.py
+++ blokshema3.py
@@ -1,1 +1,10 @@
-people = {"person1": 17}
+people = {"person1": 17, "person2": 20, "person3": 19}
+for person in people:
+    print("------")
+    print(person)  # tieku klāt vārdam
+    print(people[person])  # tieku klāt vecumam
+
+    if people[person] >= 18:
+        print(f"{person} ir pilngadīgs(-a)")
+    else:
+        print(f"{person} nav pilngadīgs(-a)")
